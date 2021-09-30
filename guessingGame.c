dcf5e5a105d85634e0b1e0a135b3cf6ce251594c
Gabrielle David <gtdavid@cpp.edu>
2021-09-30 14:56:34 -0700

    updated with code

--- guessingGame.c
+++ guessingGame.c
@@ -5,23 +5,105 @@
 // 3 to quit
 
 // include statements
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 // print menu function
+void printMenu();
 
 // main method
-// print menu
-// while userInput is not equal to 3
+int main()
+{
+   int userOption, correctNumber, newMax, maxValue = 10;
+   char userGuess[3], numAsString[3];
+   time_t t;
 
-// if userInput is equal to 1
-// prompt user to enter a number for guessing game
-// while userInput is not equal to q
-// if userInput equals correctNumber, go back to menu
-// else if userInput < correctNumber, print out "too low" and allow to guess again
-// else if userInput > correctNumber, print out "too high" and allow to guess again
+   do
+   {
+      // print menu on screen and accept user input
+      printMenu();
+      scanf("%d", &userOption);
 
-// if userInput is equal to 2
-// prompt user to set new max value
-// if userInput is negative or greater than 10, input another value
-// else, set as max value
+      // choose number guessing game
+      if (userOption == 1)
+      {
+         // generate random number for guessing game
+         srand((unsigned) time(&t));
+         correctNumber = (rand() % maxValue + 1);
+         // convert random number int to string
+         sprintf(numAsString, "%d", correctNumber);
 
-// print thank user for playing and end game
+         do
+         {
+            // prompt user to enter number for guessing game; store as string
+            printf("Please guess a number between 1 and %d: ", maxValue);
+            scanf("%s", userGuess);
+
+            // compare: if guess is equal to random number
+            if (strcmp(userGuess, numAsString) == 0)
+            {
+               printf("Congrats! You guessed the right number!\n");
+               break;
+            }
+            // compare: if guess is less than random number in ascii
+            else if (strcmp(userGuess, numAsString) < 0)
+            {
+               printf("Too low. Please try again.\n");
+            }
+            // compare: if guess is greater than random number in ascii
+            else if (strcmp(userGuess, numAsString) > 0)
+            {
+               printf("Too high. Please try again.\n");
+            }
+
+           // quit if user enters "q" 
+         } while (strcmp(userGuess, "q") != 0);
+      }
+
+      // choose to change max value
+      if (userOption == 2)
+      {
+         do
+         {
+            // prompt user to enter number for max value; store as int
+            printf("Please set a new number for the max value: ");
+            scanf("%d", &newMax);
+
+            // validate that user value is within boundaries
+            if (newMax > 0 && newMax <= maxValue)
+            {
+               maxValue = newMax;
+               printf("Max value set to %d\n", maxValue);
+               break;
+            }
+            else if (newMax <= 0)
+            {
+               printf("Max value cannot be less than zero. Please try again.\n");
+            }
+            else if (newMax > maxValue)
+            {
+               printf("Max value cannot be greater than current max value. "); 
+               printf("Please try again.\n");
+            }
+
+         } while (newMax <= 0 || newMax > maxValue);  
+      }
+   } while (userOption != 3);
+
+   // print final message to user
+   printf("\nThank you for playing the number guessing game.\n");
+   printf("The number guessing game has ended.\n\n");
+
+   return 0;
+}
+
+void printMenu()
+{
+   printf("\nMAIN MENU:\n");
+   printf("    Press 1 to play a game\n");
+   printf("    Press 2 to change the max number\n");
+   printf("    Press 3 to quit\n\n");
+   printf("Please enter your option: ");
+}
