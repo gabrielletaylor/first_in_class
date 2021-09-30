d78490707f93b23f0e1e91d81a1fc917eb399a6b
Gabrielle David <gtdavid@cpp.edu>
2021-09-29 19:57:07 -0700

    initial comments outline

---
+++ *.c
@@ -0,0 +1,22 @@
+// this program computes the tax and tip on a restaurant bill for a patron
+// accepts the tax and tip both as petcentages from command line
+// displays the meal cost, tax amount, and total bill on the screen
+// meal cost chosen randomly from 4 options
+
+// include statements
+
+// main method
+// initialize menu array of size 4
+// assign each index to corresponding meal cost
+// create variables for meal cost, tax, tip, total, time
+// display menu on screen
+// ask user to enter tax percent
+// save in variable tax
+// ask user to enter tip percent
+// save in variable tip
+// initialize random number generator
+// assign meal cost to random number index of array
+// print cost of meal
+// print tax * meal cost
+// print tip * meal cost
+// print total cost
