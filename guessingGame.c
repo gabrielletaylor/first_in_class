25ad519a135710907bc77dbf91b1f53028695d41
Gabrielle David <gtdavid@cpp.edu>
2021-09-30 02:22:05 -0700

    initial commits outline

---
+++ guessingGame.c
@@ -0,0 +1,27 @@
+// an interactive program that allows a user to guess a number between 1-10
+// prints a menu with 3 options:
+// 1 to play guessing game
+// 2 to change max number
+// 3 to quit
+
+// include statements
+
+// print menu function
+
+// main method
+// print menu
+// while userInput is not equal to 3
+
+// if userInput is equal to 1
+// prompt user to enter a number for guessing game
+// while userInput is not equal to q
+// if userInput equals correctNumber, go back to menu
+// else if userInput < correctNumber, print out "too low" and allow to guess again
+// else if userInput > correctNumber, print out "too high" and allow to guess again
+
+// if userInput is equal to 2
+// prompt user to set new max value
+// if userInput is negative or greater than 10, input another value
+// else, set as max value
+
+// print thank user for playing and end game
